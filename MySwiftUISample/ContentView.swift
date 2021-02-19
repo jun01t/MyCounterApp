423a218556677e0c8c2110df49041697b7733183
TaharaNoNeko <tmdjnch0901@gmail.com>
2021-02-19 16:20:45 +0900

    ボタンを横並びに修正。

--- MySwiftUISample/ContentView.swift
+++ MySwiftUISample/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var outputText = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            Text(String(outputText))
+                .font(.largeTitle)
+            
+            HStack {
+                Button(action: {
+                    
+                    outputText = outputText + 1
+                    
+                }) {
+                    Text("+").foregroundColor(Color.white).padding(.all).background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+                }
+                
+                Button(action: {
+                    outputText = outputText - 1
+                }) {
+                    Text("-")
+                        .foregroundColor(Color.white)
+                        .padding(.all)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+                }
+                Button(action: {
+                    outputText = 0
+                }) {
+                    Text("Clear")
+                        .foregroundColor(Color.white)
+                        .padding(.all)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+                }
+            }
+            
+        }
     }
 }
 
